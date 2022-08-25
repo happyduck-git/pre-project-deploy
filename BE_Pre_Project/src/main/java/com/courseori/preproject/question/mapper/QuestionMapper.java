30f0057ba214f8c9f6bd686442c1d52b33e2c827
GGShin <ggbetterus@gmail.com>
2022-08-25 11:43:35 +0900

    Update Test Codes

--- BE_Pre_Project/src/main/java/com/courseori/preproject/question/mapper/QuestionMapper.java
+++ BE_Pre_Project/src/main/java/com/courseori/preproject/question/mapper/QuestionMapper.java
@@ -1,7 +1,20 @@
 package com.courseori.preproject.question.mapper;
 
+import com.courseori.preproject.question.dto.QuestionDto;
+import com.courseori.preproject.question.entity.Question;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
+
+    QuestionDto.Response questionToQuestionResponse(Question question);
+
+    List<QuestionDto.Response> questionsToQuestionResponses(List<Question> questionList);
+
+    Question questionPostDtoToQuestion(QuestionDto.Post post);
+    Question questionPatchDtoTOQuestion(QuestionDto.Patch patch);
+
+
 }
